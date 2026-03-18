adf21685d283ae9920714a0da4148065cc4878b6
lenismelvin <lenismelvin@gmail.com>
2026-03-17 22:50:39 -0400

    Add files via upload

---
+++ NavesUSFX_012026GameMode.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "Enemigo.h" 
+#include "NavesUSFX_012026GameMode.generated.h"
+
+UCLASS(minimalapi)
+class ANavesUSFX_012026GameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+protected:
+	virtual void BeginPlay() override;
+
+
+public:
+	ANavesUSFX_012026GameMode();
+
+public:
+	UPROPERTY(EditAnywhere, Category = "Enemigos")
+		TSubclassOf<Aenemigo> ClaseEnemigo;
+};
